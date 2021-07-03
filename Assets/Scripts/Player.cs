08682263449062109cf221016b909e2ade00c42d
Deniz <denizsimsek93@gmail.com>
2021-07-03 10:30:35 +0300

    Add Platformer & Strafe movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,81 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    enum MovementMode
+    {
+        Platformer,
+        Strafe
+    }
+
     [SerializeField]
-    private float _moveSpeed = 5f;
+    private MovementMode _movementMode = MovementMode.Strafe;
+    [SerializeField]
+    private float _walkSpeed = 3f;
+    [SerializeField]
+    private float _runningSpeed = 6f;
     [SerializeField]
     private float _gravity = 9.81f;
+    [SerializeField]
+    private float _gravityPlatformer = -12f;
     [SerializeField]
     private float _jumpSpeed = 3.5f;
     [SerializeField]
     private float _doubleJumpMultiplier = 0.5f;
+    [SerializeField]
+    private GameObject _cameraRig;
+
+    public float jumpHeight = 1;
 
     private CharacterController _controller;
 
     private float _directionY;
+    private float _currentSpeed;
 
     private bool _canDoubleJump = false;
 
-    // Start is called before the first frame update
+    public float turnSmoothTime = 0.2f;
+    float turnSmoothVelocity;
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+
+    private float velocityY;
+
     void Start()
     {
         _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        if (_movementMode == MovementMode.Strafe)
+        {
+            MovementStafe();
+        }
+
+        if (_movementMode == MovementMode.Platformer)
+        {
+            MovementPlatformer();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (IsPlayerMoving() && _movementMode == MovementMode.Strafe)
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, _cameraRig.transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+
+    private bool IsPlayerMoving()
+    {
+        return Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0;
+    }
+
+    private void MovementStafe()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput);
 
         if (_controller.isGrounded)
         {
@@ -41,8 +88,11 @@ public class Player : MonoBehaviour
             {
                 _directionY = _jumpSpeed;
             }
-        } else {
-            if(Input.GetButtonDown("Jump") && _canDoubleJump) {
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump") && _canDoubleJump)
+            {
                 _directionY = _jumpSpeed * _doubleJumpMultiplier;
                 _canDoubleJump = false;
             }
@@ -50,8 +100,50 @@ public class Player : MonoBehaviour
 
         _directionY -= _gravity * Time.deltaTime;
 
-        direction.y = _directionY;
+        moveDirection = transform.TransformDirection(moveDirection);
 
-        _controller.Move(direction * _moveSpeed * Time.deltaTime);
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = (running) ? _runningSpeed : _walkSpeed;
+        _currentSpeed = Mathf.SmoothDamp(_currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        moveDirection.y = _directionY;
+
+        _controller.Move(_currentSpeed * Time.deltaTime * moveDirection);
+    }
+
+    private void MovementPlatformer()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+        bool running = Input.GetKey(KeyCode.LeftShift);
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + _cameraRig.transform.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+
+        float targetSpeed = ((running) ? _runningSpeed : _walkSpeed) * inputDir.magnitude;
+        _currentSpeed = Mathf.SmoothDamp(_currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        velocityY += Time.deltaTime * _gravityPlatformer;
+        Vector3 velocity = transform.forward * _currentSpeed + Vector3.up * velocityY;
+
+        _controller.Move(velocity * Time.deltaTime);
+        _currentSpeed = new Vector2(_controller.velocity.x, _controller.velocity.z).magnitude;
+
+        if (_controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_controller.isGrounded)
+            {
+                float jumpVelocity = Mathf.Sqrt(-2 * _gravityPlatformer * jumpHeight);
+                velocityY = jumpVelocity;
+            }
+        }
     }
 }
